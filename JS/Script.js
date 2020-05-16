9a1e7987643f0d1c4f10727cf3c45628b281f375
SergeyAndrienko <SergeyAndrienko1206@gmail.com>
2020-05-16 23:58:06 +0300

    finished

---
+++ JS/Script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $("#navToggle").click(function() {
+        $(this).toggleClass("active");
+        $(".overlay").toggleClass("open");
+        // this line ▼ prevents content scroll-behind
+        $("body").toggleClass("locked");
+    });
+
+
+
+
+});
